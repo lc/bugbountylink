04255af2cd896f73244a60b0e1bc85ef0cd80a38
Corben Leo <corben@lynxsecurity.io>
2019-01-16 15:35:07 -0600

    fix db

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
 CREATE USER 'bugbountylink'@'localhost' IDENTIFIED BY 'password';
 CREATE DATABASE bugbountylink;
 GRANT ALL PRIVILEGES ON bugbountylink.* To 'bugbountylink'@'localhost';
-CREATE TABLE links (id varchar(255),dest varchar(255));
+CREATE TABLE links (id varchar(255),dest varchar(1024));
